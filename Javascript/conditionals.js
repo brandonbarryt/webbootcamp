7e78c586b1f7ba2b3fd2407c5834e2c096737a15
Brandon <brandonbarrythomas@gmail.com>
2018-12-31 17:34:01 -0500

    Condition

---
+++ Javascript/conditionals.js
@@ -0,0 +1,10 @@
+var age = Number (prompt("What is your age?"));
+
+if (age < 0) {console.log("You are waaaay too young...get outt here!")};
+
+if (age === 21) {console.log("Aww snap! You can drink congrats but be eeeasy")}
+
+if (age % 2 !== 0) {console.log ("Dude you are so odd!")};
+
+if (age % Math.sqrt(age) === 0) {console.log ("You are an L7....straight square man!")}
+
