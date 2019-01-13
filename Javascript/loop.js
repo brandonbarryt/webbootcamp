500a082bf9a2b275bd2199fa20d6fee28d121200
THOMASW10A\Brandon <brandonbarrythomas@gmail.com>
2019-01-13 00:38:27 -0500

    Loop Excercice

---
+++ Javascript/loop.js
@@ -0,0 +1,25 @@
+var num = -10;
+while (num < 20) {console.log(num);
+num++;
+    
+};
+
+var num = 10;
+while (num < 41) {console.log(num);
+    num += 2;
+   
+};
+
+var num = 301;
+while (num < 334) {console.log(num);
+    num += 2;
+    
+};
+
+var num = 5 ;
+while (num <= 50) {
+    if (num % 5 === 0 && num % 3 === 0)  
+        {console.log(num);}
+    num++;
+        
+}
