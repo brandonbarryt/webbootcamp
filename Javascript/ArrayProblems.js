e3573abd11816e30da85421c338f828d114c8c6d
Brandon <brandonbarrythomas@gmail.com>
2019-03-19 17:25:37 -0400

    Working on array problems

---
+++ Javascript/ArrayProblems.js
@@ -0,0 +1,29 @@
+var array = [];
+
+var input = prompt ("What can I do for you today?");
+
+
+
+
+
+function max() {}
+
+function isUniform() {
+    var first = array[0]
+    for (var i = 1 ; i < array.length; i ++){
+        if(array[i] !== first) {
+            return false;
+        }
+    } 
+        return true;
+}
+
+function printReverse() {
+    for (var i = array.length -1; i >= 0; i--){
+        console.log(arr[i]);
+    }
+}
+
+function sumArray() {
+    
+}
--- Javascript/list.js
+++ Javascript/list.js
@@ -4,16 +4,36 @@ var input = prompt("What would you like to do?")
 
 while (input != "quit") {
     if (input === "list"){
-    console.log(todos); }
-    
-    else if (input ==="new") {
-        //ask for new todo
-        var newTodo = prompt ("Enter new todo");
-        //add to todos array
-        todos.push(newTodo);
-        //ask again        
+        listTodos()
+    }   else if (input ==="new") {
+        addTodo();
+    }   else if (input === "delete"){
+        deleteTodo();
     }
+    //ask again 
     var input = prompt("What would you like to do?");   
 
     }
     console.log ("Ok. I guess you arte done");
+
+    function listTodos(){
+        todos.forEach (function (todo, i) { 
+        console.log(i + ": " + todo); 
+        });
+        console.log ("**********")}
+
+    
+    function addTodo() {
+        //ask for new todo
+        var newTodo = prompt ("Enter new todo");
+        //add to todos array
+        todos.push(newTodo);
+        console.log ("Added Todo");}
+
+    function deleteTodo() {
+        var index = prompt ("Enter index of todo to delete");
+        //delete the todo
+        //using splice ()
+        todos.splice(index, 1);
+        console.log ("Deleted Todo");
+    }
