e7c2ba05f1449ac4bb684f9aa802cfea775892e9
Brandon <brandonbarrythomas@gmail.com>
2019-09-06 19:45:56 -0400

    Game updates with random colors

--- DOM/colorGame.js
+++ DOM/colorGame.js
@@ -7,10 +7,44 @@ var colors = [
     "rgb(255, 0, 255)",
 ]
 
-var squares = document.querySelectorAll(".square")
+var squares = document.querySelectorAll(".square");
+var pickedColor = pickColor();
+var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
+
+colorDisplay.textContent = pickedColor;
 
 for (var i =0 ; i < squares.length; i++){
+    //add initial colors
     squares[i].style.backgroundColor = colors[i];
-    
+    //add click listeners
+    squares[i].addEventListener("click",function(){
+        //get color of clicked square
+        var clickedColor = this.style.backgroundColor;
+        if (clickedColor === pickedColor){
+            messageDisplay.textContent = "Correct!";
+            changeColors(clickedColor);
+        }
+        else {
+            this.style.backgroundColor = "#232323";
+            messageDisplay.textContent = "Try Again!";
+        }
+        //compare color to picked color
+    });
 
-}
+    }
+    function changeColors(color){
+        for (var i =0 ; i < squares.length; i++){
+            squares[i].style.backgroundColor = color;
+        }
+    }
+
+    function pickColor(){
+        var random = Math.floor(Math.random() * colors.length);
+        return colors[random];
+    }
+
+   
+
+
+
