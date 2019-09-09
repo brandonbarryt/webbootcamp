24377600753b4cb365e31c8369d647c5d3084c76
THOMASW10A\Brandon <brandonbarrythomas@gmail.com>
2019-09-08 23:11:13 -0400

    Completed Color game Pt 5

--- DOM/colorGame.js
+++ DOM/colorGame.js
@@ -1,18 +1,28 @@
-var colors = [
-    "rgb(255, 0, 0)",
-    "rgb(255, 255, 0)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 255, 255)",
-    "rgb(0, 0, 255)",
-    "rgb(255, 0, 255)",
-]
-
+var colors = gernerateRandomColors(6);
+var h1  = document.querySelector("h1");
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
+var resetButton = document.querySelector("#reset");
 
 colorDisplay.textContent = pickedColor;
+
+resetButton.addEventListener("click", function(){
+    //generate all new colors
+    colors = gernerateRandomColors(6);
+    //pick a new random color
+    pickedColor = pickColor();
+    //change colors of squares
+    colorDisplay.textContent = pickedColor;
+    //change color of squares
+    for (var i =0 ; i < squares.length; i++){
+        squares[i].style.backgroundColor = colors[i];
+    };
+    h1.style.backgroundColor = "#232323";
+    resetButton.textContent = "NEW COLORS";
+
+})
 
 for (var i =0 ; i < squares.length; i++){
     //add initial colors
@@ -23,7 +33,9 @@ for (var i =0 ; i < squares.length; i++){
         var clickedColor = this.style.backgroundColor;
         if (clickedColor === pickedColor){
             messageDisplay.textContent = "Correct!";
+            resetButton.textContent = "PLAY AGAIN?";
             changeColors(clickedColor);
+            h1.style.backgroundColor = pickedColor;
         }
         else {
             this.style.backgroundColor = "#232323";
@@ -44,7 +56,29 @@ for (var i =0 ; i < squares.length; i++){
         return colors[random];
     }
 
-   
+   function gernerateRandomColors(num){
+        //make an array
+        var arr = []
+        //repeat num times
+        for (var i = 0; i < num; i++){
+        //get random colors and push
+        arr.push(randomColor());    
+        }
+
+        return arr;
+   }
+
+   function randomColor(){
+        //pick a red
+        var r = Math.floor(Math.random() * 256);
+        //pick a green
+        var g = Math.floor(Math.random() * 256);
+        //pick a blue
+        var b = Math.floor(Math.random() * 256);
+
+        return "rgb(" + r + ", " + g + ", " + b + ")";
+
+   }
 
 
 
